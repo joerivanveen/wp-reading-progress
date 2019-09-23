e85acc9e96e62b8f42fee536d6c008a4f19e5917
Joeri van Veen <nonstockphoto@gmail.com>
2019-09-23 13:15:59 +0200

    Added a few translations and a settings link on the plugin page
    Signed-off-by: Joeri van Veen <nonstockphoto@gmail.com>

--- wp-reading-progress/wp-reading-progress.php
+++ wp-reading-progress/wp-reading-progress.php
@@ -10,20 +10,16 @@ License: GPLv3
 Text Domain: ruigehond
 Domain Path: /languages
 */
-
 defined('ABSPATH') or die();
 // This is plugin nr. 6 by Ruige hond. It identifies as: ruigehond006.
 Define('RUIGEHOND006_VERSION', '1.0.3');
-
 // Register hooks for plugin management, functions are at the bottom of this file.
 register_activation_hook(__FILE__, 'ruigehond006_install');
 register_deactivation_hook(__FILE__, 'ruigehond006_deactivate');
 register_uninstall_hook(__FILE__, 'ruigehond006_uninstall');
-
 // Startup the plugin
 add_action('init', 'ruigehond006_run');
 add_action('wp', 'ruigehond006_localize');
-
 /**
  * the actual plugin on the frontend
  */
@@ -36,6 +32,7 @@ function ruigehond006_run()
         wp_enqueue_script('ruigehond006_admin_javascript', plugin_dir_url(__FILE__) . 'admin.js', 'wp-color-picker', RUIGEHOND006_VERSION, true);
         add_action('admin_init', 'ruigehond006_settings');
         add_action('admin_menu', 'ruigehond006_menuitem');
+        add_filter('plugin_action_links_' . plugin_basename(__FILE__), 'ruigehond006_settingslink'); // settings link on plugins page
     } else {
         wp_enqueue_script('ruigehond006_javascript', plugin_dir_url(__FILE__) . 'wp-reading-progress.min.js', 'jQuery', RUIGEHOND006_VERSION);
         wp_enqueue_style('ruigehond006_stylesheet', plugin_dir_url(__FILE__) . 'wp-reading-progress.min.css', false, RUIGEHOND006_VERSION);
@@ -89,13 +86,11 @@ function ruigehond006_settings()
      * - the function that will validate the options, valid options are automatically saved by WP
      */
     register_setting('ruigehond006', 'ruigehond006', 'ruigehond006_settings_validate');
-
     // register a new section in the page
     add_settings_section(
         'progress_bar_settings', // section id
         __('Set your options', 'ruigehond'), // title
         function () {
-			/* TRANSLATORS: Hi */
             echo '<p>' . __('This plugin displays a reading progress bar on your single blog post pages, using the blog article to calculate reading progress.', 'ruigehond') .
                 '<br/>' . __('Optionally you can place it on other pages as well, where it will always use the whole page to calculate reading progress.', 'ruigehond') .
                 '<br/>' . __('Please use valid input or the bar might not display.', 'ruigehond') .
@@ -106,13 +101,13 @@ function ruigehond006_settings()
     $option = get_option('ruigehond006');
     if (!$option) {
         echo '<div class="notice notice-error is-dismissible"><p>';
-        echo __('No options found, please deactivate WP Reading Progress and then activate it again.', 'ruigehond');
+        echo sprintf(__('No options found, please deactivate %s and then activate it again.', 'ruigehond'), 'Each domain a page');
         echo '</p></div>';
     } else {
         add_settings_field(
             'ruigehond006_bar_attach', // As of WP 4.6 this value is used only internally
             // use $args' label_for to populate the id inside the callback
-            'Stick the bar to this element', // title
+            __('Stick the bar to this element', 'ruigehond'), // title
             function ($args) {
                 echo '<input type="text" name="ruigehond006[bar_attach]" value="' . $args['option']['bar_attach'] . '"/>';
                 echo '<div class="ruigehond006 explanation"><em>' . sprintf(__('Use %s or %s, or any VALID selector of a fixed element where the bar can be appended to, e.g. a sticky menu.', 'ruigehond'),
@@ -129,7 +124,7 @@ function ruigehond006_settings()
         add_settings_field(
             'ruigehond006_bar_color', // As of WP 4.6 this value is used only internally
             // use $args' label_for to populate the id inside the callback
-            'Color of the progress bar', // title
+            __('Color of the progress bar', 'ruigehond'), // title
             function ($args) {
                 echo '<input type="text" name="ruigehond006[bar_color]" value="' . $args['option']['bar_color'] . '"/>';
             }, // callback
@@ -144,10 +139,10 @@ function ruigehond006_settings()
         add_settings_field(
             'ruigehond006_bar_height', // As of WP 4.6 this value is used only internally
             // use $args' label_for to populate the id inside the callback
-            'Progress bar thickness', // title
+            __('Progress bar thickness', 'ruigehond'), // title
             function ($args) {
                 echo '<input type="text" name="ruigehond006[bar_height]" value="' . $args['option']['bar_height'] . '"/>';
-                echo '<div class="ruigehond006 explanation"><em>' .sprintf(__('Thickness based on screen height is recommended, e.g. %s. But you can also use pixels, e.g. %s.', 'ruigehond'),'<a>.5vh</a>','<a>6px</a>') . '</em></div>';
+                echo '<div class="ruigehond006 explanation"><em>' . sprintf(__('Thickness based on screen height is recommended, e.g. %s. But you can also use pixels, e.g. %s.', 'ruigehond'), '<a>.5vh</a>', '<a>6px</a>') . '</em></div>';
             }, // callback
             'ruigehond006', // page id
             'progress_bar_settings', // section id
@@ -165,7 +160,7 @@ function ruigehond006_settings()
                 if (isset($args['option']['include_comments']) && $args['option']['include_comments']) {
                     echo ' checked="checked"';
                 }
-                echo '/> use whole page to calculate reading progress</label>';
+                echo '/> ' . __('use whole page to calculate reading progress', 'ruigehond') . '</label>';
             },
             'ruigehond006',
             'progress_bar_settings',
@@ -237,6 +232,14 @@ function ruigehond006_settingspage()
     echo '</form></div>';
 }
 
+function ruigehond006_settingslink($links) {
+    $url = get_admin_url() . 'options-general.php?page=wp-reading-progress';
+    $settings_link = '<a href="' . $url . '">' . __('Settings', 'ruigehond') . '</a>';
+    array_unshift($links, $settings_link);
+
+    return $links;
+}
+
 function ruigehond006_menuitem()
 {
     add_options_page(
@@ -248,11 +251,9 @@ function ruigehond006_menuitem()
     );
 }
 
-
 /**
  * plugin management functions
  */
-
 function ruigehond006_install()
 {
     if (!get_option('ruigehond006')) { // insert default settings:
